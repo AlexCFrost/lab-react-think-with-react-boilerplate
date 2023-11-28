36890b8ba6ed24dd7cfbef6ffba4cd49b19fc6b8
AlexCFrost <siva.ganesh.c18@gmail.com>
2023-11-28 10:09:17 +0530

    react

---
+++ New yarn project/src/App.jsx
@@ -0,0 +1,12 @@
+import  ReactDOM  from 'react-dom'
+import './App.css'
+
+
+
+const App = () => {
+  return (
+    <div>Hello World</div>
+  );
+}
+
+export default App
---
+++ app.js
@@ -0,0 +1,34 @@
+// const Container = () => {
+//     return React.createElement(`div`,null,`Hey kalvians!, Welcome to React Learning`,
+//     React.createElement(`div`,null,`Let's rock and roll`))
+// }
+
+
+// const container = document.getElementById('react-container')
+
+// ReactDOM.render(React.createElement(Container),container);
+
+// class ReactContainer extends React.Component{
+//     render(){
+//         return React.createElement(`div`,null,`Hey Kalvians`,
+//         React.createElement(`div`,null,`Let's rock and roll with classes`))
+//     }
+// }
+
+// const container = document.getElementById('react-container')
+
+// ReactDOM.render(React.createElement(ReactContainer),container);
+
+
+class ReactContainer extends React.Component{
+
+      render(){
+          return (
+          <div>Hello! Welcome to Kalvium   
+             <div>This is babel</div>
+         </div> )
+      };  
+  }
+
+const container = document.getElementById('react-container');
+ReactDOM.render(<ReactContainer/>,container); 
