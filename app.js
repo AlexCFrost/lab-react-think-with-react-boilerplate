ee0ac6f93cac88f7913c3168e293afbc7aedaeac
AlecCFrost <siva.ganesh.c18@gmaol.com>
2023-12-11 16:19:55 +0530

    updated

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
-// const Container = () => {
-//     return React.createElement(`div`,null,`Hey kalvians!, Welcome to React Learning`,
-//     React.createElement(`div`,null,`Let's rock and roll`))
-// }
+const Container = () => {
+    return React.createElement(`div`,null,`Hey kalvians!, Welcome to React Learning`,
+    React.createElement(`div`,null,`Let's rock and roll`))
+}
 
+const container1 = document.getElementById('react-container')
+ReactDOM.render(React.createElement(Container),container1);
 
-// const container = document.getElementById('react-container')
+// progress2
 
-// ReactDOM.render(React.createElement(Container),container);
+class ReactContainer1 extends React.Component{
+    render(){
+        return React.createElement(`div`,null,`Hey Kalvians`,
+        React.createElement(`div`,null,`Let's rock and roll with classes`))
+    }
+}
 
-// class ReactContainer extends React.Component{
-//     render(){
-//         return React.createElement(`div`,null,`Hey Kalvians`,
-//         React.createElement(`div`,null,`Let's rock and roll with classes`))
-//     }
-// }
+const container2 = document.getElementById('react-container2')
+ReactDOM.render(React.createElement(ReactContainer1),container2);
 
-// const container = document.getElementById('react-container')
+// Progress3
 
-// ReactDOM.render(React.createElement(ReactContainer),container);
-
-
-class ReactContainer extends React.Component{
+class ReactContainer2 extends React.Component{
 
       render(){
           return (
           <div>Hello! Welcome to Kalvium   
-             <div>This is babel</div>
+             <div>Let's rock and roll-This is babel</div>
          </div> )
       };  
   }
 
-const container = document.getElementById('react-container');
-ReactDOM.render(<ReactContainer/>,container); 
+const container3 = document.getElementById('react-container3');
+ReactDOM.render(<ReactContainer2/>,container3); 
