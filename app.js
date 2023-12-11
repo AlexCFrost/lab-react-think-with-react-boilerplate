f19306cabff784bc033744e6b20ef5f2215e97b1
Siva Ganesh C <139672610+AlexCFrost@users.noreply.github.com>
2023-12-11 15:50:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-// const Container = () => {
-//     return React.createElement(`div`,null,`Hey kalvians!, Welcome to React Learning`,
-//     React.createElement(`div`,null,`Let's rock and roll`))
-// }
+const Container = () => {
+    return React.createElement(`div`,null,`Hey kalvians!, Welcome to React Learning`,
+    React.createElement(`div`,null,`Let's rock and roll`))
+}
 
 
-// const container = document.getElementById('react-container')
+const container = document.getElementById('react-container')
 
-// ReactDOM.render(React.createElement(Container),container);
+ReactDOM.render(React.createElement(Container),container);
 
-// class ReactContainer extends React.Component{
-//     render(){
-//         return React.createElement(`div`,null,`Hey Kalvians`,
-//         React.createElement(`div`,null,`Let's rock and roll with classes`))
-//     }
-// }
+class ReactContainer extends React.Component{
+    render(){
+        return React.createElement(`div`,null,`Hey Kalvians`,
+        React.createElement(`div`,null,`Let's rock and roll with classes`))
+    }
+}
 
-// const container = document.getElementById('react-container')
+const container = document.getElementById('react-container')
 
-// ReactDOM.render(React.createElement(ReactContainer),container);
+ReactDOM.render(React.createElement(ReactContainer),container);
 
 
 class ReactContainer extends React.Component{
